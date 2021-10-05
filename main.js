f3c35de158d9a37c0ffe68c4b1fc562c4dd88bf9
Rajat Gupta <rajatjkg33@gmail.com>
2021-10-05 11:25:47 +0530

    Final Commit

--- main.js
+++ main.js
@@ -2,3 +2,36 @@ function select(index){
     document.getElementById('Menu').style.display = "none";
     document.getElementById("-"+index).style.display = "block";
 }
+function generate1(){
+    let number1 = document.getElementById('cc1').value + document.getElementById('phone1').value;
+    let link1 = `http://api.whatsapp.com/send?phone=${number1}`;
+    console.log(link1);
+    document.getElementById('cc1').value = '';
+    document.getElementById('phone1').value = '';
+    butt1 = document.getElementById('send1');
+    butt1.setAttribute('href',link1);
+    document.getElementById('but1').style.display = "";
+}
+
+function generate2(){
+    let number2 = document.getElementById('cc2').value+document.getElementById('phone2').value;
+    let message2 = document.getElementById('message2').value;
+    let link2 = `http://api.whatsapp.com/send?phone=${number2}text=${message2}`;
+    console.log(link2);
+    butt2 = document.getElementById('send2');
+    butt2.setAttribute('href',link2);
+    document.getElementById('cc2').value = '';
+    document.getElementById('phone2').value = '';
+    document.getElementById('message2').value = '';
+    document.getElementById('but2').style.display = "";
+}
+
+function generate3(){
+    let message3 = document.getElementById('message3').value;
+    let link3 = `http://api.whatsapp.com/send?text=${message3}`;
+    console.log(link3);
+    butt3 = document.getElementById('send3');
+    butt3.setAttribute('href',link3);
+    document.getElementById('message3').value = '';
+    document.getElementById('but3').style.display = "";
+}
